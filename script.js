8a7dc0ff4ceee44c82dfed6c87ecd15d73810de8
emyamidis <131462085+emyamidis@users.noreply.github.com>
2024-04-30 17:05:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+const button = document.getElementById('button')
+const emailInput = document.getElementById('email')
+const form = document.getElementById('form')
+let formsubmitted = false
+
+function validateEmail(element) {
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattern.test(element)
+}
+
+function setError(message) {
+    const errorDisplay = document.getElementById('error-message')
+    
+    errorDisplay.innerText = message;
+    errorDisplay.style.display = 'block';
+    emailInput.classList.add('active');
+}
+
+function setSuccess() {
+    const errorDisplay = document.getElementById('error-message')
+
+    errorDisplay.innerText = '';
+    errorDisplay.style.display = 'none';
+    emailInput.classList.remove('active');
+}
+
+function checkEmail() {
+    if (emailInput.value === '') {
+        setError('Email is required');
+    }  else if (!validateEmail(emailInput.value)) {
+        setError('Please provide a valid email address');
+    } else {
+        setSuccess();
+    };
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkEmail();
+    formsubmitted = true;
+});
+
+emailInput.addEventListener('input', () => {
+    if (formsubmitted) {
+        checkEmail();
+    }
+});
+
+})
